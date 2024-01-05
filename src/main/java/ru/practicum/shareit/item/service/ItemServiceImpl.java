df665299e7603f0bf0ec3583e58ead226960111f
DmitriiMarchenko <dmitri.marchenko96@yandex.ru>
2024-01-06 00:01:31 +0500

    unmutable map

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.mapper.ItemMapper;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.service.UserServiceImpl;
 import java.util.*;
 import java.util.stream.Collectors;
